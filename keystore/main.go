dbbcf5778069cba1fbc1039487a0b324049b73c7
Utkarsh Singh <20616980+utkarsh1097@users.noreply.github.com>
2024-04-30 11:39:09 +0900

    Minor fixes

--- keystore/main.go
+++ keystore/main.go
@@ -11,19 +11,22 @@ import (
 
 // Keystores in go-ethereum can only contain one wallet key pair per file. However, they can contain multiple files.
 func main() {
-	ks := keystore.NewKeyStore("./tmp", keystore.StandardScryptN, keystore.StandardScryptP)
+	// ks := keystore.NewKeyStore("./tmp", keystore.StandardScryptN, keystore.StandardScryptP)
 
 	// NewKeyStore at a directory with existing key files will result in their auto-load
-	// ks := keystore.NewKeyStore("./wallets", keystore.StandardScryptN, keystore.StandardScryptP)
+	ks := keystore.NewKeyStore("./wallets", keystore.StandardScryptN, keystore.StandardScryptP)
 
 	password := "yadayadahogehoge"
-	// account, err := ks.NewAccount(password)
-	// if err != nil {
-	// 	log.Fatal("Unable to create new account")
+	account, err := ks.NewAccount(password)
+	if err != nil {
+		log.Fatal("Unable to create new account")
+	}
 
-	// }
+	fmt.Println(account.Address.Hex()) // 0x20F8D42FB0F667F2E53930fed426f225752453b3
 
-	currAddrPath := "UTC--2024-04-22T02-04-51.580627000Z--fa2d187a4552ab44272955610f909934b36072e2"
+	fmt.Println(ks.Wallets())
+
+	currAddrPath := "UTC--2024-04-24T16-20-38.368197000Z--cdadc9ba28b9565151d5f087a40a91bd58c8bb1b"
 
 	fileData, err := ioutil.ReadFile(path.Join("wallets", currAddrPath))
 	if err != nil {
@@ -33,12 +36,13 @@ func main() {
 	fmt.Println(ks.Accounts())
 	fmt.Println("OK")
 
-	account, err := ks.Import(fileData, password, password)
+	_, err = ks.Import(fileData, password, password)
 	if err != nil {
 		log.Fatalf("Unable to import data. Details: %v\n", err)
 	}
 
-	fmt.Println(account.Address.Hex()) // 0x20F8D42FB0F667F2E53930fed426f225752453b3
+	fmt.Println(ks.Accounts())
+	fmt.Println("OK")
 
 	// if err := os.Remove(path.Join("./wallets/tmp", currAddrPath)); err != nil {
 	// 	log.Fatal(err)
